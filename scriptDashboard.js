6a2fb5c0424ed72f7a276637ab35905e16546b30
TJ <tahmeenaowais@yahoo.com>
2020-08-04 13:56:16 -0400

    final commit

--- scriptDashboard.js
+++ scriptDashboard.js
@@ -3,9 +3,9 @@ displayRecentSearchHistory();
 
 const state = {};
 var now = moment();
-var date = now.format("MM/DD/YYYY");
+var date = now.format("LL");
 console.log(date)
-var time = now.format("HH:mm");
+var time = now.format("h:mm A");
 //Date and time displayed
 $("#currentDay").append(date);
 $("#currentTime").append(time)
@@ -25,7 +25,7 @@ $("#saveBtn").on("click", function () {
   $(".card-text10").empty()
   $("#humidity").empty()
    $("#windspeed").empty()
-    $("#temprature").empty()
+    $("#Temp").empty()
     $("#uvindex").empty()
   var city = $("#autocomplete-input").val()
   console.log(city)
@@ -72,144 +72,144 @@ function getWeather(city){
       // Log the resulting object
       console.log(response);
  
-      $("#cityName").append("Current Location: " + city.toUpperCase())
-      var humidity = response.list[0].main.humidity
+      $("#cityName").append("Location: " + city.toUpperCase()).css({ 'font-weight':'bold'});
+      var humidity =response.list[0].main.humidity
       var wind = response.list[0].wind.speed
       var temp = (((response.list[0].main.temp) - 273.15) * 9) / 5 + 32;
- $("#humidity").append("Humidity: " + humidity + "%")
-   $("#windspeed").append("Wind Speed: " + wind);
-    $("#temprature").append("Temprature: " + temp.toFixed(2));
+ $("#humidity").append("Humidity: " + humidity + "%").css({ 'font-weight':'bold'});
+   $("#windspeed").append("Wind Speed: " + wind).css({ 'font-weight':'bold'});
+    $("#temprature").append("Temp: " + temp.toFixed(0)+"°F").css({ 'font-weight':'bold'});
     $("#image").attr("src", ("http://openweathermap.org/img/wn/" + response.list[0].weather[0].icon + "@2x.png"))
 
       var oneDaysForward = moment().add(1, 'day');
-      $(".card-text1").append("Date: " + oneDaysForward.format("MM/DD/YYYY"));
+      $(".card-text1").append(oneDaysForward.format("LL")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[8].main.humidity
       var pressure = response.list[8].main.pressure
       var temp = (((response.list[8].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[8].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
       var info2 = $("<li>").append("Pressure: " + pressure);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
 $("#image1").attr("src", ("http://openweathermap.org/img/wn/" + response.list[8].weather[0].icon + "@2x.png"))
       $(".card-text1").append( info1, info2, info3, info4);
 
       var twoDaysForward = moment().add(2, 'day');
-      $(".card-text2").append("Date: " + twoDaysForward.format("MM/DD/YYYY"));
+      $(".card-text2").append(twoDaysForward.format("LL")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[16].main.humidity
       var pressure = response.list[16].main.pressure
       var temp = (((response.list[16].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[16].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
       var info2 = $("<li>").append("Pressure: " + pressure);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image2").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text2").append(info1, info2, info3, info4);
 
       var threeDaysForward = moment().add(3, 'day');
-      $(".card-text3").append("Date: " + threeDaysForward.format("MM/DD/YYYY"));
+      $(".card-text3").append(threeDaysForward.format("LL")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[24].main.humidity
       var pressure = response.list[24].main.pressure
       var temp = (((response.list[24].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[24].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
       var info2 = $("<li>").append("Pressure: " + pressure);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image3").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text3").append(info1, info2, info3, info4);
 
       var fourDaysForward = moment().add(4, 'day');
-      $(".card-text4").append("Date: " + fourDaysForward.format("MM/DD/YYYY"));
+      $(".card-text4").append(fourDaysForward.format("LL")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[32].main.humidity
       var pressure = response.list[32].main.pressure
       var temp = (((response.list[32].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[32].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
       var info2 = $("<li>").append("Pressure: " + pressure);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0+"°F"));
       $("#image4").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text4").append(info1, info2, info3, info4);
 
       var fiveDaysForward = moment().add(5, 'day');
-      $(".card-text5").append("Date: " + fiveDaysForward.format("MM/DD/YYYY"));
+      $(".card-text5").append(fiveDaysForward.format("LL")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[39].main.humidity
       var pressure = response.list[39].main.pressure
       var temp = (((response.list[39].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[39].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
       var info2 = $("<li>").append("Pressure: " + pressure);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image5").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text5").append(info1, info2, info3, info4);
 
       var fourhrsForward = moment().add(4, 'hours');
-      $(".card-text6").append("At: " + fourhrsForward.format("h A"));
+      $(".card-text6").append(fourhrsForward.format("h A")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[0].main.humidity
       var wind = response.list[0].wind.speed
       var temp = (((response.list[0].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[0].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
-      var info2 = $("<li>").append("Wind Speed: " + wind);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info2 = $("<li>").append("Win Speed: " + wind);
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image6").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text6").append(info1, info2, info3, info4);
 
 
       var eighthrsForward = moment().add(8, 'hours');
-      $(".card-text7").append("At: " + eighthrsForward.format("h A"));
+      $(".card-text7").append( eighthrsForward.format("h A")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[2].main.humidity
       var wind = response.list[2].wind.speed
       var temp = (((response.list[2].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[2].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
-      var info2 = $("<li>").append("Wind Speed: " + wind);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info2 = $("<li>").append("Win Speed: " + wind);
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image7").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text7").append(info1, info2, info3, info4);
 
       var twelvehrsForward = moment().add(12, 'hours');
-      $(".card-text8").append("At: " + twelvehrsForward.format("h A"));
+      $(".card-text8").append( twelvehrsForward.format("h A")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[4].main.humidity
       var wind = response.list[4].wind.speed
       var temp = (((response.list[4].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[4].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
-      var info2 = $("<li>").append("Wind Speed: " + wind);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info2 = $("<li>").append("Win Speed: " + wind);
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image8").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text8").append(info1, info2, info3, info4);
 
       var sixteenhrsForward = moment().add(16, 'hours');
-      $(".card-text9").append("At: " + sixteenhrsForward.format("h A"));
+      $(".card-text9").append( sixteenhrsForward.format("h A")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[5].main.humidity
       var wind = response.list[5].wind.speed
       var temp = (((response.list[5].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[5].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
-      var info2 = $("<li>").append("Wind Speed: " + wind);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info2 = $("<li>").append("Win Speed: " + wind);
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image9").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text9").append(info1, info2, info3, info4);
 
 
       var twentyhrsForward = moment().add(20, 'hours');
-      $(".card-text10").append("At: " + twentyhrsForward.format("h A"));
+      $(".card-text10").append(twentyhrsForward.format("h A")).css({ 'font-weight':'bolder', 'text-align':'center'});
       var humidity = response.list[7].main.humidity
       var pressure = response.list[7].main.pressure
       var temp = (((response.list[7].main.temp) - 273.15) * 9) / 5 + 32;
       var feels_like = (((response.list[7].main.feels_like) - 273.15) * 9) / 5 + 32;
       var info1 = $("<li>").append("Humidity: " + humidity + "%")
-      var info2 = $("<li>").append("Wind Speed: " + wind);
-      var info3 = $("<li>").append("Temprature: " + temp.toFixed(2));
-      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(2));
+      var info2 = $("<li>").append("Win Speed: " + wind);
+      var info3 = $("<li>").append("Temp: " + temp.toFixed(0)+"°F");
+      var info4 = $("<li>").append("Feels Like: " + feels_like.toFixed(0)+"°F");
       $("#image10").attr("src", ("http://openweathermap.org/img/wn/" + response.list[16].weather[0].icon + "@2x.png"))
       $(".card-text10").append(info1, info2, info3, info4);
    
@@ -236,7 +236,7 @@ $('#list').on('click', 'li', function(){
   $(".card-text10").empty()
   $("#humidity").empty()
    $("#windspeed").empty()
-    $("#temprature").empty()
+    $("#Temp").empty()
     $("#uvindex").empty()
       console.log(city)
       getWeather(city)
@@ -290,10 +290,10 @@ function uvIndex(lat, long){
     var uvindex= parseInt(response2.current.uvi)
   $("#uvindex").append("UV Index: " + uvindex)
 if (uvindex >=0 && uvindex< 3){
-  $("#uvindex").css({'display': 'block', 'background-color' : 'green'});
+  $("#uvindex").css({'color' : 'green', 'font-weight':'bold'});
 }
-else if (uvindex > 3 && uvindex < 10){
-  $("#uvindex").css({'display': 'block', 'background-color' : 'yellow'});
+else if (uvindex > 3 && uvindex < 11){
+  $("#uvindex").css({'color' : 'yellow', 'font-weight':'bold'});
 
 }
 else{
